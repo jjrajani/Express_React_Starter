caaecdd62340140b140a26c0dee46e440172f021
humdrum <jjrajani@gmail.com>
2017-04-15 12:17:16 -0400

    added routes to get users, get user, post user

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 a crete-react-app with express wired
+
+npm install
+npm run build
+node server
--- server/app.js
+++ server/app.js
@@ -1,15 +1,83 @@
 // server/app.js
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 // Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
+app.get('/:page', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
+
+var USERS = [
+  {id: 0, username: "", password: "", role: "", loggedIn: false},
+  {id: 1, username: "humdrum", password: "humdrum", role: "admin", loggedIn: false},
+]
+
+let compileUsers = function (req, res, next) {
+  let users = USERS.map((user) => {
+    return {
+      username: user.username
+    }
+  }).slice(1, USERS.length);
+  res.body = { users }
+  next();
+}
+
+app.get('/api/users', compileUsers, function (req, res) {
+  console.log("GET USERS LIST", res.body);
+  res.status(200).json(res.body);
+});
+
+var authorize = function (req, res, next) {
+  console.log("Authorizing: ", req.headers);
+  var authed = false;
+  var index;
+  USERS.forEach((user, i) => {
+    if (user.username === req.headers.username
+    && user.password === req.headers.password) {
+      authed = true;
+      index = i;
+    }
+  });
+  if (authed) {
+    var user = USERS[index]
+    res.body = { user };
+    next();
+  } else {
+    res.status(422).json("Invalid username password combination");
+  }
+  console.log("authed", authed)
+  next();
+}
+app.get('/api/user/:name', authorize, (req, res) => {
+  console.log("GET.res.body", res.body);
+  res.status(200).json({
+    username: res.body.user.username,
+    loggedIn: true,
+  });
+});
+
+var validate = function (req, res, next) {
+  let valid = true;
+  USERS.forEach((user) => {
+    if (user.username === req.body.username) {
+      valid = false;
+    }
+  });
+  if (valid) {
+    next();
+  } else {
+    res.status(422).json("Username already exists");
+  }
+}
+app.post('/api/user', validate, (req, res) => {
+  console.log("POST.res.body", res.body);
+  res.send("User POST request made")
+})
 
 module.exports = app;
